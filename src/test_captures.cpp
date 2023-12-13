15221c4645b0e37a3147c722296e308f91f5d99a
matryt <mathieu.cuvelier@outlook.fr>
2023-12-13 21:30:37 +0100

    Test captures d'images

---
+++ src/test_captures.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mathi on 13/12/2023.
+//
+
+#include "test_captures.h"
---
+++ src/test_captures.h
@@ -0,0 +1,14 @@
+//
+// Created by mathi on 13/12/2023.
+//
+
+#ifndef PROJET_TEST_CAPTURES_H
+#define PROJET_TEST_CAPTURES_H
+
+
+class test_captures {
+
+};
+
+
+#endif //PROJET_TEST_CAPTURES_H

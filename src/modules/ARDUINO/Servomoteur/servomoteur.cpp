0d77dac051d259c17a2f7e1e8599f174cbaade17
Mathieu CUVELIER <mathieu.cuvelier@outlook.fr>
2024-02-10 18:27:19 +0100

    Changement servo

--- src/modules/ARDUINO/Servomoteur/servomoteur.cpp
+++ src/modules/ARDUINO/Servomoteur/servomoteur.cpp
@@ -8,8 +8,22 @@ Servo servo;
 
 void Servomoteur::init() {
     servo.attach(PINS::SERVO);
+    int cpt = 0;
+    int max = (SERVO::MAX-ANGLE - SERVO::MIN_ANGLE) * 2 / SERVO::INCREMENT;
 }
 
 void Servomoteur::write_angle(int angle) {
     servo.write(angle);
+}
+
+void Servomoteur::next() {
+    cpt = cpt % 30;
+    if (cpt >= max/2) {
+        angle = (max - cpt) * SERVO::INCREMENT;
+    }
+    else {
+        angle = cpt * SERVO::INCREMENT;
+    }
+    write_angle(SERVO::MIN_ANGLE + angle)
+    cpt++;
 }

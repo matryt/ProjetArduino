3fbcdcec20b4be5f3f5dff1573d88dc7ba08442c
Mathieu CUVELIER <mathieu.cuvelier@outlook.fr>
2024-02-17 15:25:18 +0100

    Bouton reload

--- src/modules/COMPUTER/main/static/scripts/reloadPage.js
+++ src/modules/COMPUTER/main/static/scripts/reloadPage.js
@@ -1,5 +1,40 @@
+// Variable globale pour stocker l'ID de l'intervalle
+var intervalId = null;
+
 function reload() {
     window.location.reload();
 }
 
-//setInterval(reload, 1000 * 5)
+// Fonction pour démarrer l'intervalle
+function startInterval() {
+    intervalId = setInterval(reload,  5000);
+}
+
+// Fonction pour arrêter l'intervalle
+function stopInterval() {
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+}
+
+// Fonction pour initialiser l'intervalle lorsque la page est chargée
+function initializeInterval() {
+    // Vérifie si la checkbox est cochée et démarre l'intervalle
+    var checkbox = document.getElementById("reload");
+    if (checkbox.checked) {
+        startInterval();
+    }
+}
+
+// Écouteur d'événements pour la checkbox
+document.getElementById("reload").addEventListener("change", function() {
+    if (this.checked) {
+        startInterval();
+    } else {
+        stopInterval();
+    }
+});
+
+// Démarre l'intervalle lorsque la page est chargée
+window.addEventListener("load", initializeInterval);
